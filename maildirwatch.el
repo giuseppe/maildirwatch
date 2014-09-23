5615e27d3dee7fba7f11b873bd278ab992ba90e3
Giuseppe Scrivano <gscrivano@gnu.org>
2014-09-23 12:09:54 +0200

    maildirwatch.el: print the message subject

--- maildirwatch.el
+++ maildirwatch.el
@@ -27,12 +27,25 @@
 
 (setq-default maildirwatch-path "/usr/bin/maildirwatch")
 
-(defun maildirwatch-filter (proc output)
-  (message (concat "new message " output)))
+(defun maildirwatch-desc (file)
+  (with-current-buffer (find-file-literally file)
+    (unwind-protect
+        (let ((start (re-search-forward "Subject:+ " nil t)))
+          (if start
+              (progn (end-of-line)
+                     (buffer-substring start (point)))
+            file))
+      (kill-buffer (current-buffer)))))
+
+(defun maildirwatch-filter (proc file)
+  (message (concat "new message "
+                   (maildirwatch-desc
+                    (replace-regexp-in-string "\n$" "" file)))))
 
 (defun maildirwatch-watch (maildirs)
   (interactive)
   (let* ((mdirs-path (mapcar 'expand-file-name maildirwatch-mdirs))
-        (proc (apply 'start-process "maildirwatch" nil maildirwatch-path mdirs-path)))
-    (set-process-filter proc maildirwatch-filter)
+        (proc (apply 'start-process "maildirwatch" nil
+                     maildirwatch-path mdirs-path)))
+    (set-process-filter proc 'maildirwatch-filter)
     proc))
