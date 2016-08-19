2bab1960382602f696a62757393353cd47efc14d
Giuseppe Scrivano <gscrivano@gnu.org>
2016-08-19 16:28:24 +0200

    Fix compiler warning
    Signed-off-by: Giuseppe Scrivano <gscrivano@gnu.org>

--- maildirwatch.c
+++ maildirwatch.c
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with Wget.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+#define _GNU_SOURCE
 
 #include <errno.h>
 #include <poll.h>
@@ -24,6 +25,7 @@ along with Wget.  If not, see <http://www.gnu.org/licenses/>.
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <error.h>
 
 #include <dirent.h>
 #include <sys/types.h>
